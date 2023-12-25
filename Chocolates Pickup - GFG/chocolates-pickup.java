ca4696be1a79e1b1dae9b0f1058bec0ed42625b4
harshitkhare44 <99740751+harshitkhare44@users.noreply.github.com>
2023-12-25 14:58:03 +0530

    Added solution - LeetHub

--- Chocolates Pickup - GFG/chocolates-pickup.java
+++ Chocolates Pickup - GFG/chocolates-pickup.java
@@ -35,41 +35,41 @@ class GFG{
 class Solution{
     public int solve(int n, int m, int grid[][]){
         // Code here
-        int dp[][][]=new int[n][m][m];
-       for(int j1=0;j1<m;j1++){
-           for(int j2=0;j2<m;j2++){
-               if(j1==j2)
-               dp[n-1][j1][j2]=grid[n-1][j2];
-               else 
-               dp[n-1][j1][j2]=grid[n-1][j1]+grid[n-1][j2];
-           }
-       }
-      
-       for(int i=n-2;i>=0;i--){
-           for(int j1=0;j1<m;j1++){
-               for(int j2=0;j2<m;j2++){
-                   int maxi=(int)-1e9;
-                    int del[]={-1,0,1};
-                    for(int d1=0;d1<3;d1++){
-                        for(int d2=0;d2<3;d2++){
-                            int value=0;
-                            if(j1==j2){
-                                value=grid[i][j1];
-                            }
-                            else
-                            value=grid[i][j1]+grid[i][j2];
-                            if(j1+del[d1]>=0 && j1+del[d1]<m && j2+del[d2]>=0 && j2+del[d2]<m){
-                                value+=dp[i+1][j1+del[d1]][j2+del[d2]];
-                            }
-                            else value=(int)-1e9;
-                            maxi=Math.max(maxi,value);
-                        }
-                    }
-                    dp[i][j1][j2]=maxi;
-               }
-           }
-          
-       }
-        return dp[0][0][m-1];
+     int prev[][]=new int[m][m];
+     for(int j1=0;j1<m;j1++){
+         for(int j2=0;j2<m;j2++){
+             if(j1==j2)
+             prev[j1][j2]=grid[n-1][j1];
+             else
+             prev[j1][j2]=grid[n-1][j1]+grid[n-1][j2];
+         }
+     }
+     for(int i=n-2;i>=0;i--){
+         int temp[][]=new int[m][m];
+         for(int j1=0;j1<m;j1++){
+             for(int j2=0; j2<m;j2++){
+                 int del[]={-1,0,1};
+                 int maxi=(int)-1e9;
+                 for(int d1=0;d1<3;d1++){
+                     for(int d2=0;d2<3;d2++){
+                         int value=0;
+                         if(j1==j2){
+                             value+=grid[i][j1];
+                         }
+                         else value+=grid[i][j1]+grid[i][j2];
+                         if(j1+del[d1]>=0 && j1+del[d1]<m && j2+del[d2]>=0 && j2+del[d2]<m){
+                             value+=prev[j1+del[d1]][j2+del[d2]];
+                         }
+                         else value=(int)-1e9;
+                         maxi=Math.max(value,maxi);
+                        
+                     }
+                 }
+                 temp[j1][j2]=maxi;
+             }
+         }
+         prev=temp;
+     }
+     return prev[0][m-1];
     }
 }
